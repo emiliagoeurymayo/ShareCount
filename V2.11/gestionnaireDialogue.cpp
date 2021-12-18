dcb4dbf696a96be9cb2686c0f53fea8935ae3ea9
thibaultodor <60348757+thibaultodor@users.noreply.github.com>
2021-12-18 19:44:50 +0100

    V2.11

---
+++ V2.11/gestionnaireDialogue.cpp
@@ -0,0 +1,214 @@
+#include <string>
+#include <list>
+#include <iostream>
+#include "gestionnaireDialogue.h"
+#include "shareCount.h"
+#include "serveurMail.h"
+#include <limits>
+
+/** @brief Classe GestionnaireDialogue
+ **
+ ** Modele de notre application MVC
+ **
+ ** @version 2.4
+ **
+ ** @author Thibault Odor, Marie-Luc Moselle, Emilia Goeury-Mayo
+ **/
+
+///@brief Constructeur
+///
+///@param shareCount : sharecount
+///@param serveurMail : serveur mail
+///@param gbdd : gestionnaire de la base de donnée
+GestionnaireDialogue::GestionnaireDialogue(ShareCount shareCount, ServeurMail serveurMail,gestionnaireBDD gbdd):m_sharecount(shareCount),m_serveurmail(serveurMail),m_gbdd(gbdd)
+{
+
+}
+
+///@brief Constructeur vide
+GestionnaireDialogue::GestionnaireDialogue()
+{
+
+}
+
+///@brief Destructeur
+GestionnaireDialogue::~GestionnaireDialogue(){}
+
+///@brief Verifie la véracité des informations saisies dans les champs de texte de l'interface Inscription
+///
+/// @param nom le nom a attribuer au compte
+/// @param prenom le prenom a attribuer au compte
+/// @param email l'email a attribuer au compte
+/// @param mdp le mot de passe a attribuer au compte
+/// @param pseudo le pseudo a attribuer au compte
+/// @param mailvalide permet de simuler la validation ou non de l'adresse email lors de l'inscription
+void GestionnaireDialogue::verifInformations(std::string nom,std::string email,std::string mdp,std::string prenom,std::string pseudo,bool mailvalide){
+    if(!nom.empty() && !email.empty() && !mdp.empty() && !prenom.empty() && !pseudo.empty()){
+        if(m_serveurmail.is_valid(email)){
+            m_nom=nom;
+            m_prenom=prenom;
+            m_email=email;
+            m_pseudo=pseudo;
+            m_mdp=mdp;
+            this->envoiEmail(mailvalide);
+        }
+        else{
+            std::cout << "eMail saisi invalide" << std::endl;
+        }
+    }
+    else{
+        std::cout << "Un des champs est invalide (EmptyError)" << std::endl;
+    }
+}
+
+///@brief "Envoie" le mail de validation
+///
+/// @param mailvalide permet de simuler la validation ou non de l'adresse email lors de l'inscription
+void GestionnaireDialogue::envoiEmail(bool mailvalide){
+    if(mailvalide){
+        this->m_serveurmail.ajouterEmail(m_email);
+        verifValidationEmail();
+    }
+    else {
+        std::cout << "Veuillez valider votre mail\n";
+    }
+}
+
+///@brief Ajoute le compte à la base de donnée aprés la vérification
+void GestionnaireDialogue::verifValidationEmail(){
+    if(m_serveurmail.getStatusEmail(m_email)){
+        if(!m_gbdd.addUtil(m_prenom,m_nom,m_email,m_mdp,"ABC")){
+            qDebug() << "eMAil déja associe a un compte";
+        }
+        else{
+            m_sharecount.ajouterCompte(m_nom,m_prenom,m_email,m_mdp,m_pseudo);
+        }
+    }
+}
+
+///@brief Retourne l'id de l'utilisateur defini à l'aide de la base de donnée, 0 sinon
+///
+/// @param string email : email à vérifier
+/// @param string mdp : mot de passe associé à l'email
+/// @return l'id de l'utilisateur defini à l'aide de la base de donnée, 0 sinon
+int GestionnaireDialogue::verifConnexion(std::string email,std::string mdp){
+    m_email = email;
+    m_mdp = mdp;
+    return m_gbdd.informationConnexionValide(email,mdp);
+}
+
+///@brief Identifie le nom et prénom de l'utilisateur possédant l'id défini en paramètre
+///
+/// @param int id : id de l'utilisateur à identifier
+/// @return retourne le nom et prénom de l'utilisateur possédant l'id défini en paramètre
+QMap<QString,QString> GestionnaireDialogue::identifierUtilisateur(int id){
+    return m_gbdd.getUtil(id);
+}
+
+///@brief Getter des participants d'un compte partagé
+///
+/// @param int typeCompte : id du type de compte à vérifier
+/// @param int idCompte : id du compte à vérifier
+/// @return retourne une liste contenant les participants d'un compte partagé
+QMap<QString,QString> GestionnaireDialogue::getParticipants(int typeCompte, int idCompte){
+    return m_gbdd.getParticipants(typeCompte, idCompte);
+}
+
+///@brief Retourne le nom du compte identifié par l'id en paramètre
+///
+/// @param int id : id du compte à identifier
+/// @return retourne le nom du compte identifier par l'id en paramètre
+QString GestionnaireDialogue::getNomCompte(int id){
+    return m_gbdd.getNomCompte(id);
+}
+
+///@brief Retourne le nom de la cagnotte identifiée par l'id en paramètre
+///
+/// @param int id : id de la cagnotte à identifier
+/// @return retourne le nom du compte identifier par l'id en paramètre
+QString GestionnaireDialogue::getNomCagnotte(int id){
+    return m_gbdd.getNomCagnotte(id);
+}
+
+
+///@brief Retourne la liste des comptes auquel l'utilisateur participe
+///
+/// @return retourne une liste contenant les comptes auquel l'utilisateur participe
+QMap<int, QString> GestionnaireDialogue::getListeCompte(){
+    return m_gbdd.getListeCompte(m_email);
+}
+
+///@brief Retourne la liste des comptes auquel l'utilisateur participe
+///
+/// @return retourne une liste contenant les comptes auquel l'utilisateur participe
+QMap<int, QString> GestionnaireDialogue::getListeCagnotte(){
+    return m_gbdd.getListeCagnotte(m_email);
+}
+
+///@brief Ajoute un participant à un compte partagé
+///
+/// @param QString email : participant à ajouter
+/// @param int idCompte : id du compte à vérifier
+/// @return retourne True si l'operation c'est bien effectuée
+bool GestionnaireDialogue::addPartCompt(QString email, int idCompte){
+    return m_gbdd.addPartCompt(email,idCompte);
+
+}
+
+///@brief Ajoute un participant à une cagnotte
+///
+/// @param QString email : participant à ajouter
+/// @param int idCagnotte : id du compte à vérifier
+/// @return retourne True si l'operation c'est bien effectuée
+bool GestionnaireDialogue::addPartCagnotte(QString email, int idCagnotte){
+    return m_gbdd.addPartCagnotte(email, idCagnotte);
+}
+
+///@brief Retourne une liste d'actions effectuées en rapport avec la cagnotte
+///
+/// @param int idCagnotte : id de la cagnotte en question
+QStringList GestionnaireDialogue::getListeHistorique(int idCagnotte){
+    return this->m_gbdd.getListeHistorique(idCagnotte);
+}
+
+///@brief Retourne les fonds disponibles d'une cagnotte
+///
+/// @param int idCagnotte : id de la cagnotte en question
+int GestionnaireDialogue::getFondsDispo(int idCagnotte){
+    return m_gbdd.getFondsDispo(idCagnotte);
+}
+
+///@brief Ajoute de l'argent à la cagnotte
+///
+/// @param fond : montant à rajouter
+/// @param m_id : id de l'utilisateur
+/// @param m_idcagnotte : id de la cagnotte
+bool GestionnaireDialogue::addFonds(QString fond, int m_id, int m_idcagnotte){
+    return m_gbdd.addFonds(fond, m_id, m_idcagnotte);
+}
+
+///@brief Ajoute de l'argent à la cagnotte
+///
+/// @param m_id : id de l'utilisateur
+/// @param m_idcagnotte : id de la cagnotte
+bool GestionnaireDialogue::retirerFonds(QString fond, int m_id, int m_idcagnotte){
+    return m_gbdd.retirerFonds(fond, m_id, m_idcagnotte);
+
+}
+
+QStringList GestionnaireDialogue::getListeDettes(int m_idcompte){
+    return m_gbdd.getListeDettes(m_idcompte);
+}
+
+bool GestionnaireDialogue::addDettes(int idcompte,int idutil1,int idutil2,int montant){
+    return m_gbdd.addDettes(idcompte,idutil1,idutil2,montant);
+}
+
+void GestionnaireDialogue::ajouterCompteP(QString nom,QString id){
+    m_gbdd.addComptePartage(nom.toStdString(),id.toStdString());
+}
+
+void GestionnaireDialogue::ajouterCagnotte(QString nom,QString id){
+    m_gbdd.addCagnotte(id.toInt(),0,nom.toStdString(),id.toStdString());
+}
+
---
+++ V2.11/windows.cpp
@@ -0,0 +1,312 @@
+#include "windows.h"
+#include "ui_windows.h"
+
+/** @brief Classe Windows
+ **
+ ** classe de la fenetre principale
+ **
+ ** @version 2.8
+ **
+ ** @author Thibault Odor, Marie-Luc Moselle, Emilia Goeury-Mayo
+ **/
+
+///@brief Constructeur
+///
+///@param parent : parent = null
+windows::windows(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::windows)
+{
+    ui->setupUi(this);
+}
+
+///@brief Destructeur
+windows::~windows()
+{
+    delete ui;
+}
+
+///@brief Slot s'activant lorsque le bouton retour dans la page accueil utilisateur est cliqué
+void windows::on_retour_accueil_clicked(){
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+///@brief Slot s'activant lorsque le bouton retour dans la page cagnotte est cliqué
+void windows::on_retour_cagnotte_clicked(){
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+///@brief Slot s'activant lorsque le bouton retour dans la page compte partagé est cliqué
+void windows::on_retour_comptepartage_clicked(){
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+///@brief Permet de definir le modele dans ConnexionInscription
+///
+/// @param GestionnaireDialogue * g : Pointeur du modele
+void windows::attachGestionnaireDialogue(GestionnaireDialogue * g){
+    m_gestionnaireDialogue=*g;
+}
+
+///@brief Identifie l'utilisateur pour l'affichage graphique de AccueilUtilisateur
+///
+/// @param QMap contenant la liste des compte identifié
+void windows::identifierUtilisateur(QMap <QString, QString> q){
+    m_utilisateur=q;
+    QString prenom;
+    for(const auto &e : m_utilisateur.toStdMap()){prenom.append(e.first);}
+    ui->NomPrenom->setText("Bonjour, "+prenom);
+    m_modelCompte = new QStringListModel(this);
+    QMap<int, QString> compte = m_gestionnaireDialogue.getListeCompte();
+    QStringList cmp;
+    for(const auto &e : compte.toStdMap()){
+        cmp.append(QString::fromStdString(std::to_string(e.first))+"."+e.second);
+    }
+    m_modelCagnotte = new QStringListModel(this);
+    QMap<int, QString> cagnotte = m_gestionnaireDialogue.getListeCagnotte();
+    QStringList cag;
+    for(const auto &e : cagnotte.toStdMap()){
+        cag.append(QString::fromStdString(std::to_string(e.first))+"."+e.second);
+    }
+    if(!cmp.isEmpty()){
+        m_modelCompte->setStringList(cmp);
+        ui->listComptePartage->setModel(m_modelCompte);
+    }
+    if(!cag.isEmpty()){
+        m_modelCagnotte->setStringList(cag);
+        ui->listCagnotte->setModel(m_modelCagnotte);
+    }
+}
+
+///@brief Slot s'activant lorsque un élément de la liste de compte partagee est cliquée
+///
+/// @param QModelIndex index de l'element cliqué
+void windows::on_listComptePartage_clicked(const QModelIndex &index)
+{
+    QString stcstring = index.data(Qt::DisplayRole).toString();
+    int id = stcstring.split(".")[0].toInt();
+    this->m_idCompte = id;
+
+    m_model_listeparticipant_compte = new QStringListModel(this);
+    QMap <QString, QString> map = m_gestionnaireDialogue.getParticipants(1, this->m_idCompte);
+    QStringList stc;
+        for(const auto &e : map.toStdMap()){
+            stc.append(e.first+" "+e.second);
+        }
+    m_model_listeparticipant_compte->setStringList(stc);
+
+    m_model_listeDettes = new QStringListModel(this);
+    QStringList dettes = m_gestionnaireDialogue.getListeDettes(m_idCompte);
+    m_model_listeDettes->setStringList(dettes);
+
+    QString str = m_gestionnaireDialogue.getNomCompte(this->m_idCompte);
+    ui->liste_compte->setModel(m_model_listeparticipant_compte);
+    ui->listeDettes->setModel(m_model_listeDettes);
+    ui->nomCompte->setText(str);
+    ui->stackedWidget->setCurrentIndex(5);
+}
+
+///@brief Slot s'activant lorsque un élément de la liste des cagnottes est cliquée
+///
+/// @param QModelIndex index de l'element cliqué
+void windows::on_listCagnotte_clicked(const QModelIndex &index){
+    QString stcstring = index.data(Qt::DisplayRole).toString();
+    int id = stcstring.split(".")[0].toInt();
+
+    m_model_listeparticipant_cagnotte = new QStringListModel(this);
+    this->m_idCagnotte = id;
+    QMap <QString, QString> map = m_gestionnaireDialogue.getParticipants(2, this->m_idCagnotte);
+    QStringList stc;
+        for(const auto &e : map.toStdMap()){stc.append(e.first+" "+e.second);}
+    m_model_listeparticipant_cagnotte->setStringList(stc);
+
+    m_model_listeHistorique = new QStringListModel(this);
+    QStringList histo = m_gestionnaireDialogue.getListeHistorique(m_idCagnotte);
+    m_model_listeHistorique->setStringList(histo);
+
+    QString str = m_gestionnaireDialogue.getNomCagnotte(this->m_idCagnotte);
+    ui->liste_cagnotte->setModel(m_model_listeparticipant_cagnotte);
+    ui->liste_Historique->setModel(m_model_listeHistorique);
+    ui->nomCagnotte->setText(str);
+    ui->fondDispo->setText("Fonds disponibles : "+QString::number(m_gestionnaireDialogue.getFondsDispo(this->m_idCagnotte))+"€");
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
+///@brief Slot s'activant lorsque le bouton d'ajout de participant à une cagnotte est cliqué
+void windows::on_addPart_cagnotte_clicked()
+{
+    QString email = QInputDialog::getText(this, "Input dialog",
+                                            "Adresse Email du nouveau participant", QLineEdit::Normal);
+    qDebug() <<"result addPartCompt" << m_gestionnaireDialogue.addPartCagnotte(email, this->m_idCagnotte);
+    QMap <QString, QString> map = m_gestionnaireDialogue.getParticipants(1,this->m_idCagnotte);
+    QStringList stc;
+    for(const auto &e : map.toStdMap()){stc.append(e.first+" "+e.second);}
+    m_model_listeparticipant_cagnotte->setStringList(stc);
+    ui->liste_cagnotte->setModel(m_model_listeparticipant_cagnotte);
+    this->show();
+}
+
+///@brief Slot s'activant lorsque le bouton d'ajout de participant à un compte partagé
+void windows::on_addPart_compte_clicked()
+{
+    QString email = QInputDialog::getText(this, "Input dialog","Adresse Email du nouveau participant", QLineEdit::Normal);
+
+    qDebug() <<"result addPartCompt" <<m_gestionnaireDialogue.addPartCompt(email, this->m_idCompte);
+
+    QMap <QString, QString> map = m_gestionnaireDialogue.getParticipants(1,this->m_idCompte);
+    QStringList stc;
+    for(const auto &e : map.toStdMap()){stc.append(e.first+" "+e.second);}
+    m_model_listeparticipant_compte->setStringList(stc);
+    ui->liste_compte->setModel(m_model_listeparticipant_compte);
+    this->show();
+}
+
+
+///@brief Retourne le texte qui se situe dans le champs Mail de l'interface de Connexion
+///
+/// @return retourne le texte qui se situe dans le champs Mail de l'interface de Connexion
+QString windows::getLineEditeMail(){return ui->lineEdit_eMail->text();}
+
+///@brief Retourne le texte qui se situe dans le champs Mdp de l'interface de Connexion
+///
+/// @return retourne le texte qui se situe dans le champs Mdp de l'interface de Connexion
+QString windows::getLineEditMdp(){return ui->lineEdit_Mdp->text();}
+
+///@brief Retourne le texte qui se situe dans le champs Nom de l'interface d'inscription
+///
+/// @return retourne le texte qui se situe dans le champs Nom de l'interface d'inscription
+QString windows::getLineEditNom(){return ui->lineEdit_Nom->text();}
+
+///@brief Retourne le texte qui se situe dans le champs Mail de l'interface d'inscription
+///
+/// @return retourne le texte qui se situe dans le champs Mail de l'interface d'inscription
+QString windows::getLineEditeMailInscription(){return ui->lineEdit_eMail_inscription->text();}
+
+///@brief Retourne le texte qui se situe dans le champs Mdp de l'interface d'inscription
+///
+/// @return retourne le texte qui se situe dans le champs Mdp de l'interface d'inscription
+QString windows::getLineEditMdpInscription(){return ui->lineEdit_Mdp_inscription->text();}
+
+///@brief Retourne le texte qui se situe dans le champs Prenom de l'interface d'inscription
+///
+/// @return retourne le texte qui se situe dans le champs Prenom de l'interface d'inscription
+QString windows::getLineEditPrenom(){return ui->lineEdit_Prenom->text();}
+
+///@brief Retourne le texte qui se situe dans le champs Pseudo de l'interface d'inscription
+///
+/// @return retourne le texte qui se situe dans le champs Pseudo de l'interface d'inscription
+QString windows::getLineEditPseudo(){return ui->lineEdit_Pseudo->text();}
+
+///@brief Slot s'activant lorsque le bouton Connexion est cliqué
+void windows::on_boutton_connexion_clicked()
+{
+    std::string mail,mdp;
+    mail=this->getLineEditeMail().toStdString();
+    mdp=this->getLineEditMdp().toStdString();
+    m_id = m_gestionnaireDialogue.verifConnexion(mail,mdp);
+    if(m_id != 0 ){
+        identifierUtilisateur(m_gestionnaireDialogue.identifierUtilisateur(m_id));
+        ui->stackedWidget->setCurrentIndex(3);
+    }
+}
+
+///@brief Slot s'activant lorsque le bouton retour dans la page de Connexion est cliqué
+void windows::on_retourAccueil_connexion_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+///@brief Slot s'activant lorsque le bouton retour dans la page d'Inscription est cliqué
+void windows::on_retourAccueil_inscription_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+///@brief Slot s'activant lorsque le bouton créer un compte dans la page d'Inscription est cliqué
+void windows::on_boutton_Inscription_clicked()
+{
+    std::string nom,mail,mdp,pseudo,prenom;
+    nom=this->getLineEditNom().toStdString();
+    mail=this->getLineEditeMailInscription().toStdString();
+    mdp=this->getLineEditMdpInscription().toStdString();
+    prenom=this->getLineEditPrenom().toStdString();
+    pseudo=this->getLineEditPseudo().toStdString();
+    m_gestionnaireDialogue.verifInformations(nom,mail,mdp,prenom,pseudo,ui->buttonmailvalide->isChecked());
+}
+
+///@brief Slot s'activant lorsque le bouton inscription dans la page de Connexion/Inscription est cliqué
+void windows::on_bouttonInscription_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+///@brief Slot s'activant lorsque le bouton connexion dans la page de Connexion/Inscription est cliqué
+void windows::on_bouttonConnexion_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+///@brief Slot s'activant lorsque le bouton d'ajout de fond est cliqué
+void windows::on_addFond_clicked(){
+    QString fond = QInputDialog::getText(this, "Input dialog","Fonds à ajouter", QLineEdit::Normal);
+    if(fond.toInt()>0){
+        if(m_gestionnaireDialogue.addFonds(fond, m_id, m_idCagnotte)){
+            ui->fondDispo->setText("Fonds disponibles : "+QString::number(m_gestionnaireDialogue.getFondsDispo(this->m_idCagnotte))+"€");
+            QStringList histo = m_gestionnaireDialogue.getListeHistorique(m_idCagnotte);
+            m_model_listeHistorique->setStringList(histo);
+            ui->liste_Historique->setModel(m_model_listeHistorique);
+        }
+    }
+}
+
+///@brief Slot s'activant lorsque le bouton de retrait de fond cliqué
+void windows::on_retirerFond_clicked(){
+    QString fond = QInputDialog::getText(this, "Input dialog","Fonds à retirer", QLineEdit::Normal);
+    if(fond.toInt()>0){
+        if(m_gestionnaireDialogue.retirerFonds(fond, m_id, m_idCagnotte)){
+            ui->fondDispo->setText("Fonds disponibles : "+QString::number(m_gestionnaireDialogue.getFondsDispo(this->m_idCagnotte))+"€");
+            QStringList histo = m_gestionnaireDialogue.getListeHistorique(m_idCagnotte);
+            m_model_listeHistorique->setStringList(histo);
+            ui->liste_Historique->setModel(m_model_listeHistorique);
+        }
+    }
+}
+
+
+void windows::on_addDep_clicked(){
+    QModelIndex index = ui->liste_compte->currentIndex();
+    QString stcstring = index.data(Qt::DisplayRole).toString();
+    int id = stcstring.split(".")[0].toInt();
+    if(id!=0 && id!=m_id){
+        QString montant = QInputDialog::getText(this, "Input dialog","Montant de votre dette", QLineEdit::Normal);
+            if(m_gestionnaireDialogue.addDettes(m_idCompte, m_id, id, montant.toInt())){
+                m_model_listeDettes = new QStringListModel(this);
+                QStringList dettes = m_gestionnaireDialogue.getListeDettes(m_idCompte);
+                m_model_listeDettes->setStringList(dettes);
+                ui->listeDettes->setModel(m_model_listeDettes);
+            }
+    }
+}
+
+
+void windows::on_creerCompte_clicked()
+{
+    QString nom = QInputDialog::getText(this, "Input dialog","Nom à attribuer à votre compte", QLineEdit::Normal);
+    QString id;
+    if(!nom.isEmpty()){
+    m_gestionnaireDialogue.ajouterCompteP(nom,id.number(m_id));
+    identifierUtilisateur(m_gestionnaireDialogue.identifierUtilisateur(m_id));
+    }
+}
+
+
+void windows::on_creerCagnotte_clicked()
+{
+    QString nom = QInputDialog::getText(this, "Input dialog","Nom à attribuer à votre cagnotte", QLineEdit::Normal);
+    QString id;
+    if(!nom.isEmpty()){
+    m_gestionnaireDialogue.ajouterCagnotte(nom,id.number(m_id));
+    identifierUtilisateur(m_gestionnaireDialogue.identifierUtilisateur(m_id));
+    }
+}
+
